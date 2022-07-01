caf7fe4c5cde59463fc23612d68205fc5abe194a
Austin Gae <austingae@gmail.com>
2022-06-30 23:05:19 -0700

    How to get one file to be displayed on the website page. only one page, though

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav>
+        <div className='nav-bar'>
+          <div className='nav-bar__left-section'>
+            <h2 className='nav-bar__website-title'>Austin Busting Website</h2>
+          </div>
+          <div className='nav-bar__right-section'>
+            <a className='nav-bar__link nav-bar__link--margin-right' href='/about'>About Me</a>
+            <a className='nav-bar__link nav-bar__link--margin-right' href=''>Projects</a>
+            <a className='nav-bar__link' href=''>Contact Me</a>
+          </div>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  )
+
 }
 
 export default MyApp
---
+++ posts/post1.md
@@ -0,0 +1,12 @@
+---
+title: Portfolio Website
+slug: home
+excerpt: This is Austin Gae's portfolio website to display his projects.
+coverImage: /images/profile.jpg
+---
+
+## Hello! I am Austin Gae.
+
+![Austin Gae](/images/profile.jpg)
+
+I am a front-end web developer, who specializes in HTML, CSS, Javascript, and ReactJS to create awesome websites. I am also comfortable with Git and GitHub to work with other developers in a team. Now, don't just take my word for it, take a look at my projects!
